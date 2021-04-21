527dbceeb1e3ee6ec2138d09e3d2e066091d01a5
Vitor Oliveira Ropke <60607636+vitorropke@users.noreply.github.com>
2021-04-21 12:07:02 -0300

    Sort

--- Scripts_Python/scriptGetCoordenadas/getCoordenadas.py
+++ Scripts_Python/scriptGetCoordenadas/getCoordenadas.py
@@ -26,7 +26,6 @@ while True:
 	if mapa[indice] == '/' and mapa[indice + 1] == 'n' and mapa[indice + 2] == 'o' and mapa[indice + 3] == 'd' and mapa[indice + 4] == 'e' and mapa[indice + 5] == 's':
 		break
 """
-
 strCoordenadas = ""
 
 # Procura pelas coordenadas e salva na string
@@ -51,6 +50,54 @@ while True:
 
 	if mapa[indice] == '/' and mapa[indice + 1] == 'n' and mapa[indice + 2] == 'o' and mapa[indice + 3] == 'd' and mapa[indice + 4] == 'e' and mapa[indice + 5] == 's':
 		break
+
+# obtém as coordenadas da network para salvar em um vetor
+# Coleta o número de coordenadas
+numeroCoordenadas = 0
+indice = 0
+while True:
+	if strCoordenadas[indice] == '\n':
+		numeroCoordenadas += 1
+	#Incrementa o índice. Se for o último dígito o loop pára
+	indice += 1
+	if indice == len(strCoordenadas):
+		break
+
+# Inicializa vetor de coordenadas
+floatCoordenadas = [[0 for y in range(2)] for x in range(numeroCoordenadas)]
+
+# Insere as coordenadas no vetor
+indice = 0
+for x in range(numeroCoordenadas):
+	strNumeroAtual = ""
+	while strCoordenadas[indice] != '\t':
+		strNumeroAtual += strCoordenadas[indice]
+		indice += 1
+
+	floatCoordenadas[x][0] = float(strNumeroAtual)
+	strNumeroAtual = ""
+	indice += 1
+
+	while strCoordenadas[indice] != '\n':
+		strNumeroAtual += strCoordenadas[indice]
+		indice += 1
+
+	floatCoordenadas[x][1] = float(strNumeroAtual)
+	indice += 1
+
+# Ordena o vetor de coordenadas
+floatCoordenadas.sort()
+"""
+for x in range(numeroCoordenadas):
+	print(floatCoordenadas[x][0])
+strCoordenadas = ""
+"""
+# Vetor para string
+for x in range(numeroCoordenadas):
+	strCoordenadas += str(floatCoordenadas[x][0])
+	strCoordenadas += '\t'
+	strCoordenadas += str(floatCoordenadas[x][1])
+	strCoordenadas += '\n'
 
 #Criando arquivo de horários
 arquivoCoordenadas = open("coordenadas.txt", "w")
